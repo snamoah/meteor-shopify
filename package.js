30d23dde2af866627eb709d04cb045b6772f7500
Samuel Amoah <snamoah@gmail.com>
2016-04-16 11:31:23 +0000

    change package name

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'shopify',
+  name: 'snamoah:shopify',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Shopify OAuth flow',
   // URL to the Git repository containing the source code for this package.
-  //git: '',
+  git: 'https://github.com/snamoah/meteor-shopify',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
